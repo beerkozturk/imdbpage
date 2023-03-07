47e6bf664ce91e25bb6e070b0e392538d9fc96d8
Berk Öztürk <berkozturk@Berk-MBP.modem.local>
2023-03-07 23:32:24 +0300

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,67 @@
+import React from "react";
+import MovieList from './MovieList';
+import SearchBar from './SearchBar'
+
+class App extends React.Component {
+
+    state = {
+        movies: [],
+        searchQuery: ""
+
+    }
+    async componentDidMount() {
+        const baseURL = "http://localhost:3002/movies";
+        const response = await fetch(baseURL);
+        console.log(response)
+        const data = await response.json();
+        console.log(data)
+        this.setState({ movies: data })
+    }
+    deleteMovie = (movie) => {
+        const newMovieList = this.state.movies.filter(
+            m => m.id !== movie.id
+        );
+
+        /*    this.setState(
+               {
+                   movies: newMovieList
+               }
+           ) */
+
+        this.setState(state => ({
+            movies: newMovieList
+        }))
+
+    }
+
+    searchMovie = (event) => {
+        // console.log(event.target.value)
+        this.setState({ searchQuery: event.target.value })
+    }
+
+    render() {
+
+        let filterdMovies = this.state.movies.filter(
+            (movie) => {
+                return movie.name.toLowerCase().indexOf(this.state.searchQuery.toLowerCase()) !== -1
+            }
+        )
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-12">
+                        <SearchBar searchMovieProp={this.searchMovie} />
+                    </div>
+                </div>
+
+                <MovieList
+                    movies={filterdMovies}
+                    deleteMovieProp={this.deleteMovie} />
+            </div>
+        )
+    }
+}
+
+export default App;
+
+
